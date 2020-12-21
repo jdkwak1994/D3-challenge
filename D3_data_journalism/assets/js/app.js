af0e1a4b0358ab3d2104dadc863c7507bb12230e
jdkwak1994 <jdkwak1994@gmail.com>
2020-12-21 00:31:51 -0500

    added axis and reading csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,41 @@
-// @TODO: YOUR CODE HERE!
+var margin = 10;
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+d3.csv("assets/data/data.csv").then(function(csvdata) {
+    
+    csvdata.forEach(function(data) {
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+    });
+
+    //console.log(csvdata);
+
+    var xscale = d3
+        .scaleLinear()
+        .domain([0, 100])
+        .range([0, 100]);
+    var yscale = d3
+        .scaleLinear()
+        .domain([0, 100])
+        .range([0, 100]);
+
+    var xaxis = d3.axisBottom(xscale);
+    var yaxis = d3.axisLeft(yscale);
+    
+    svg
+        .append("g")
+        .call(xaxis)
+        .attr("class", "xaxis");
+    svg
+        .append("g")
+        .call(yaxis)
+        .attr("class", "yaxis");
+
+    
+
+});
